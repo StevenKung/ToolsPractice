330c3fd8bfccf3dacf0a4859213923b21634aa0a
Steven <s790408p@gmail.com>
2019-04-29 15:21:33 +0800

    use RelayCommand in CommandWpf

--- RecipeSystem/FileSystemViewModel.cs
+++ RecipeSystem/FileSystemViewModel.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.CommandWpf;
 
 namespace RecipeSystem
 {
