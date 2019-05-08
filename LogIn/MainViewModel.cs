ab3788b939c8de651d9bc0eb5d6a9a5931738be3
StevenKung <s790408p@gmail.com>
2019-05-08 23:03:12 +0800

    catch

--- LogIn/MainViewModel.cs
+++ LogIn/MainViewModel.cs
@@ -1,19 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
 
 namespace LogIn
 {
-	public class MainViewModel
+	public class MainViewModel : INotifyPropertyChanged
 	{
-		object m_CurrentPage;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+		public ICommand LoginPageCommand { get; set; }
+		public ICommand RegisterPageCommand { get; set; }
+
+		object m_CurrentPage { get; set; }
 		public object CurrentPage => m_CurrentPage;
+		private object LoginPage = new LoginViewModel();
+		private object RegisterPage = new RegisterViewModel();
 		public MainViewModel()
 		{
-			m_CurrentPage = new LoginViewModel();
+			m_CurrentPage = LoginPage;
+			LoginPageCommand = new RelayCommand(()=> m_CurrentPage = LoginPage);
+			RegisterPageCommand = new RelayCommand(()=>m_CurrentPage = RegisterPage);
+
 		}
 	}
 }
---
+++ LogIn/UserData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogIn
+{
+	public class UserData
+	{
+		public string Account { get; set; }
+		public string Password { get; set; }
+		public accessLevelEnum Level { get; set; }
+		public UserData(string account, string password, accessLevelEnum level)
+		{
+			Account = account;
+			Password = password;
+			Level = level;
+		}
+	}
+
+	public enum accessLevelEnum
+	{
+		DEVELOPER = 0x100,
+		SERVICE = 0x10,
+		END_USER = 0x1
+	}
+}
