8c0c37aea1a8c433a5fe071a78872b799f318a52
Steven <s790408p@gmail.com>
2019-05-08 18:01:40 +0800

    catch

---
+++ LogIn/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LogIn
+{
+	public class MainViewModel
+	{
+		object m_CurrentPage;
+		public object CurrentPage => m_CurrentPage;
+		public MainViewModel()
+		{
+			m_CurrentPage = new LoginViewModel();
+		}
+	}
+}
